794dac2fab3afd67821af791c25971853e7fe701
Taffarel Xavier <taffarelxavier7@gmail.com>
2019-06-17 22:28:45 -0300

    sda

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ app.use('/produto', produtoRouter);
 app.use('/usuario', usuarioRouter);
 
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
